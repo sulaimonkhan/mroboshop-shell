370d2553cbc67ce5ab871e5a5f9e5a2b61d246e0
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-15 22:57:56 +0530

    aaply

--- dispatch.sh
+++ dispatch.sh
@@ -1,30 +1,33 @@
-echo -e "\e[35m copy dispatch service file \e[0m"
+color="\e[35m"
+no_color="\e[0m"
+
+echo -e "$color copy dispatch service file $no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
 
-echo -e "\e[35m Install Golang \e[0m"
+echo -e "$color Install Golang $no_color"
 dnf install golang -y
 
-echo -e "\e[35m Add Application user \e[0m"
+echo -e "$color Add Application user $no_color"
 useradd roboshop
 
-echo -e "\e[35m Create Application Directory \e[0m"
+echo -e "$color Create Application Directory $no_color"
 rm -rf /app
 mkdir /app 
 
-echo -e "\e[35m Download Application Content \e[0m"
+echo -e "$color Download Application Content $no_color"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip 
 cd /app 
 
-echo -e "\e[35m Extract Application Content \e[0m"
+echo -e "$color Extract Application Content $no_color"
 unzip /tmp/dispatch.zip
 
-echo -e "\e[35m Download Application Dependiences \e[0m"
+echo -e "$color Download Application Dependiences $no_color"
 cd /app 
 go mod init dispatch
 go get 
 go build
 
-echo -e "\e[35m Start Application Service \e[0m"
+echo -e "$color Start Application Service $no_color"
 systemctl daemon-reload
 systemctl enable dispatch 
 systemctl restart dispatch
--- payment.sh
+++ payment.sh
@@ -1,16 +1,34 @@
+color="\e[35m"
+no_color="\e[0m"
+
+
+echo -e "$color copy payment service file $no_color"
 cp payment.service /etc/systemd/system/payment.service
 
+echo -e "$color Install python $no_color"
 dnf install python3 gcc python3-devel -y
 
+echo -e "$color Add Application user $no_color"
 useradd roboshop
+
+echo -e "$color create application directory $no_color"
+rm -rf /app
 mkdir /app 
 
+echo -e "$color Download application content $no_color"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip 
 cd /app 
+
+echo -e "$color Extract application content $no_color"
 unzip /tmp/payment.zip
 
+
+echo -e "$color Download application dependencies $no_color"
 cd /app 
 pip3 install -r requirements.txt
 
+
+echo -e "$color start application service  $no_color"
+systemctl daemon-reload
 systemctl enable payment 
 systemctl restart payment
