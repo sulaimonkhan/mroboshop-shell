271b3d44db816de51e6cba3d052611b4b2390848
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-19 19:41:41 +0530

    now ready

--- dispatch.sh
+++ dispatch.sh
@@ -1,24 +1,24 @@
 source common.sh
 app_name=dispatch
 
-print_heading "$color copy dispatch service file" 
+print_heading "copy dispatch service file" 
 cp dispatch.service /etc/systemd/system/dispatch.service &>>$log_file 
 status_check $?
 
 
-print_heading "$color Install Golang"
+print_heading "Install Golang"
 dnf install golang -y &>>$log_file
 status_check $?
 
 app_prerequisites
 
-print_heading "$color Download Application Dependiences" 
+print_heading "Download Application Dependiences" 
 go mod init dispatch &>>$log_file
 go get &>>$log_file
 go build &>>$log_file
 status_check $?
 
-print_heading "$color Start Application Service"
+print_heading "Start Application Service"
 systemctl daemon-reload &>>$log_file
 systemctl enable dispatch &>>$log_file
 systemctl restart dispatch &>>$log_file
