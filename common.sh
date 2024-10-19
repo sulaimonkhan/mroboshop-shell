82ca47fef03aebffc994f3cfb0d736dad4c0390a
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-19 19:16:34 +0530

    if conditions

--- common.sh
+++ common.sh
@@ -6,26 +6,34 @@ rm -f $log_file
 app_prerequisites() {
   print_heading "$color Add Application user" 
   useradd roboshop &>>$log_file
-  echo $?
+  status_check $?
 
   print_heading "$color Create Application Directory"
   rm -rf /app &>>$log_file
   mkdir /app  &>>$log_file
-  echo $?
+  status_check $?
 
 
   print_heading "$color Download Application Content"
   curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
-  echo $?
+  status_check $?
 
   cd /app
 
   print_heading "$color Extract Application Content"
   unzip /tmp/$app_name.zip &>>$log_file
-  echo $?
+  status_check $?
 }
 
 print_heading () {
   echo -e "$color $1 $no_color" &>>$log_file
   echo -e "$color $1 $no_color" 
+}
+
+status_check () {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else 
+    echo -e "\e[31m  FAILURE \e[0m"
+  fi
 }
--- dispatch.sh
+++ dispatch.sh
@@ -3,11 +3,12 @@ app_name=dispatch
 
 print_heading "$color copy dispatch service file" 
 cp dispatch.service /etc/systemd/system/dispatch.service &>>$log_file 
-echo $?
+status_check $?
+
 
 print_heading "$color Install Golang"
 dnf install golang -y &>>$log_file
-echo $?
+status_check $?
 
 app_prerequisites
 
@@ -15,11 +16,11 @@ print_heading "$color Download Application Dependiences"
 go mod init dispatch &>>$log_file
 go get &>>$log_file
 go build &>>$log_file
-echo $?
+status_check $?
 
 print_heading "$color Start Application Service"
 systemctl daemon-reload &>>$log_file
 systemctl enable dispatch &>>$log_file
 systemctl restart dispatch &>>$log_file
-echo $?
+status_check $?
 
