55fc41c0c2e0d3de07e005200095490b91bf7e65
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-20 23:02:19 +0530

    now

--- catalogue.sh
+++ catalogue.sh
@@ -2,7 +2,7 @@ source common.sh
 app_name=catalogue
 
 
-nodjes_setup
+nodejs_setup
 
 print_heading "Copy MongoDB repo file"
 cp $scripts_path/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
